789ee6d52754003c710819bbf080c139f11f51cf
Camila Arce <camilarce2710@gmail.com>
2024-01-17 10:44:36 -0300

    nuevos componentes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,14 +3,20 @@
      <CuentaRegresiva class="items" />
       <Lugar class="items" />
       <Fotos class="items" />
+      <Regalos class="items" />
+      <Confirmar class="items" />
+      <Hashtag class="items" />
   </div>
  
 </template>
 
 <script setup>
+import Confirmar from '@/components/Confirmar.vue';
 import CuentaRegresiva from '@/components/CuentaRegresiva.vue';
 import Fotos from '@/components/Fotos.vue';
+import Hashtag from '@/components/Hashtag.vue';
 import Lugar from '@/components/Lugar.vue';
+import Regalos from '@/components/Regalos.vue';
 </script>
 
 <style scoped>
