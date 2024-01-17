46baa011f8df54a7144852808f2191d4c5ed6969
camilaarce <camilarce2710@gmail.com>
2024-01-17 15:36:11 -0300

    transitions regalos

--- src/components/Regalos.vue
+++ src/components/Regalos.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="container">
-    <h1>Regalos</h1>
-    <h3 class="text-center mb-5">
-      ¿Te gustaría regalarme algo más, aparte de tu maravillosa presencia?
-    </h3>
+    <div class="regalo"></div>
+    <Transition name="zoom" mode="out-in">
+      <h1 v-if="transitionRegalos">Regalos</h1>
+    </Transition>
+    <Transition name="fade" mode="out-in">
+      <h3 v-if="transitionRegalos" class="text-center mb-5">
+        ¿Te gustaría regalarme algo más, aparte de tu maravillosa presencia?
+      </h3>
+    </Transition>
     <v-dialog width="600">
       <template v-slot:activator="{ props }">
-        <v-btn
-          size="x-large"
-          variant="flat"
-          color="#003b6e"
-          v-bind="props"
-          append-icon="mdi-gift"
-          >regalos</v-btn
+        <Transition name="slide" mode="out-in">
+          <v-btn
+            v-if="transitionRegalos"
+            size="x-large"
+            variant="flat"
+            color="#003b6e"
+            v-bind="props"
+            append-icon="mdi-gift"
+            >regalos</v-btn
+          ></Transition
         >
       </template>
       <template v-slot:default="{ isActive }">
@@ -24,7 +32,7 @@
               size="100"
             ></v-icon>
             <h2>Lo más importante para mí es tu presencia</h2>
-            <h2>pero si deseas hacernme un regalo aquí te dejo los datos</h2>
+            <h2>pero si deseas hacerme un regalo aquí te dejo los datos</h2>
             <h2 class="mt-3">CBU: {{ evento.cbu }}</h2>
             <h2>Alias: {{ evento.alias }}</h2>
             <h2>{{ evento.titular }}</h2>
@@ -40,6 +48,37 @@
 
 <script setup>
 import evento from "@/mocks/eventos.json";
+
+import { ref, onMounted } from "vue";
+
+const transitionRegalos = ref(false);
+
+const observer = ref(null);
+
+onMounted(() => {
+  setInterval(() => {
+    const options = {
+      root: null,
+      rootMargin: "0px",
+      threshold: 0.5,
+    };
+
+    const handleIntersection = (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          transitionRegalos.value = true;
+          observer.value.disconnect();
+        } else {
+          transitionRegalos.value = false;
+        }
+      });
+    };
+
+    observer.value = new IntersectionObserver(handleIntersection, options);
+
+    observer.value.observe(document.querySelector(".regalo"));
+  }, 1000);
+});
 </script>
 
 <style scoped>
@@ -91,5 +130,41 @@ h3 {
   75% {
     transform: translateX(-6px) rotate(-1.2deg);
   }
+}
+
+.zoom-enter-active,
+.zoom-leave-active {
+  transition: all 1s ease;
+}
+
+.zoom-enter-from,
+.zoom-leave-to {
+  opacity: 0;
+  filter: blur(10px);
+  transform: scale(0);
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 1s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+  filter: blur(10px);
+  transform: translateX(-100px);
+}
+
+.slide-enter-active,
+.slide-leave-active {
+  transition: all 1s ease;
+}
+
+.slide-enter-from,
+.slide-leave-to {
+  opacity: 0;
+  filter: blur(10px);
+  transform: translateY(-100%);
 }
 </style>
